0ac09ebd243e81812c27cc6863fb1faea27ef85b
Paranjyoti Mohanty <paranjyoti.mohanty@gmail.com>
2024-12-17 02:53:41 +0530

    replacing one lambda expression with an anonymous class for better understanding

--- LambdaPractice02.java
+++ LambdaPractice02.java
@@ -1,6 +1,12 @@
 public class LambdaPractice02 {
     public static void main(String[] args) {
-        StringFunction exclaim = s -> s + "!";
+        //StringFunction exclaim = s -> s + "!";
+        // Instead of using lambda expressions, we can write a anonymous class for implementing explain
+        StringFunction exclaim = new StringFunction() {
+            public String run(String str) {
+                return str + "!";
+            }
+        };
         StringFunction ask = s -> s + "?";
         printFormatted("Hello", exclaim);
         printFormatted("Hello", ask);

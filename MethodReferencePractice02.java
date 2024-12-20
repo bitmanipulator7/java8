cf1530681159ed2bcd257a01c8108b5e31afb2bd
Paranjyoti Mohanty <paranjyoti.mohanty@gmail.com>
2024-12-21 00:37:36 +0530

    Method Reference

---
+++ MethodReferencePractice02.java
@@ -0,0 +1,19 @@
+public class MethodReferencePractice02 {
+    public static void main(String[] args) {
+        MethodReferencePractice02 obj = new MethodReferencePractice02();
+        Sayable2 sayable = obj::doSomething; // referring the non-static method using an object
+        sayable.say();
+
+        Sayable2 sayable2 = new MethodReferencePractice02()::doSomething;
+        // referring non-static method using anonymous object
+        sayable2.say();
+    }
+
+    public void doSomething() {
+        System.out.println("I am a non-static / instance method.");
+    }
+}
+
+interface Sayable2 {
+    void say();
+}

fc0f18f51fbbb6e623ed48a40a1e81557e52d132
Paranjyoti Mohanty <paranjyoti.mohanty@gmail.com>
2024-12-23 00:53:00 +0530

    Using predefined functional interface to refer methods

---
+++ MethodReference04.java
@@ -0,0 +1,16 @@
+import java.util.function.BiFunction;
+// Here, we use predefined functional interface BiFunction to refer methods.
+// we are using BiFunction interface and using it's apply() method
+public class MethodReference04 {
+    public static void main(String[] args) {
+        BiFunction<Integer, Integer, Integer> adder = Arithmetic::add;
+        int result = adder.apply(3, 5);
+        System.out.println(result);
+    }
+}
+
+class Arithmetic {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}

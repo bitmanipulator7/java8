2a0a5713ef6342ea24a717e822049bdc16dba95e
Paranjyoti Mohanty <paranjyoti.mohanty@gmail.com>
2024-12-30 23:59:05 +0530

    Java 8 Date Time

--- DateTimePractice01.java
+++ DateTimePractice01.java
@@ -1,5 +1,8 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class DateTimePractice01 {
     public static void main(String[] args) {
@@ -9,5 +12,9 @@ public class DateTimePractice01 {
         //Creating local date by providing input arguments
         LocalDate firstDayOf2024 = LocalDate.of(2024, Month.JANUARY, 1);
         System.out.println("firstDayOf2024: " + firstDayOf2024);
+
+        LocalDateTime todayIndiaDateTime = LocalDateTime.now(ZoneId.of("Asia/Kolkata"));
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMM dd, yyyy HH:mm:ss");
+        System.out.println("Current date time in India: " + dtf.format(todayIndiaDateTime));
     }
 }

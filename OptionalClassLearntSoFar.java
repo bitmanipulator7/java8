245ded640069be07959cf762e05aeacd750107cf
Paranjyoti Mohanty <paranjyoti.mohanty@gmail.com>
2025-01-03 23:58:56 +0530

    Optional Class Practice

---
+++ OptionalClassLearntSoFar.java
@@ -0,0 +1,15 @@
+import java.util.Optional;
+
+public class OptionalClassLearntSoFar {
+    public static void main(String[] args) {
+        String[] str = new String[10];
+        str[4] = "This is a not null value";
+        Optional<String> checkNull = Optional.ofNullable(str[4]);
+        checkNull.ifPresent(System.out::println);
+        System.out.println(checkNull.get());
+
+        Optional<String> checkNullAgain = Optional.ofNullable(str[9]);
+        checkNullAgain.ifPresent(n -> System.out.println(n));
+    }
+
+}

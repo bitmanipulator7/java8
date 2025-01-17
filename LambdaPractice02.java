ecd7c20a6c2e525e18080908086b6ee65607ac81
Paranjyoti Mohanty <paranjyoti.mohanty@gmail.com>
2025-01-17 23:58:44 +0530

    Lambda Expressions for Functional Interface

--- LambdaPractice02.java
+++ LambdaPractice02.java
@@ -1,15 +1,16 @@
-public class LambdaPractice02 {
+class LambdaPractice02 {
     public static void main(String[] args) {
         //StringFunction exclaim = s -> s + "!";
-        // Instead of using lambda expressions, we can write a anonymous class for implementing explain
         StringFunction exclaim = new StringFunction() {
             public String run(String str) {
                 return str + "!";
             }
         };
         StringFunction ask = s -> s + "?";
+        StringFunction smile = n -> n + " :) ";
         printFormatted("Hello", exclaim);
         printFormatted("Hello", ask);
+        printFormatted("Hello", smile);
     }
 
     public static void printFormatted(String str, StringFunction format) {

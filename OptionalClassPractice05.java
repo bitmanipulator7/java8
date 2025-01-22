3e849e9f97cab71f506c3879623e7ba82ec099e2
Paranjyoti Mohanty <paranjyoti.mohanty@gmail.com>
2025-01-23 00:38:54 +0530

    Optional class practice

--- OptionalClassPractice05.java
+++ OptionalClassPractice05.java
@@ -1,8 +1,6 @@
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class OptionalClassPractice05 {
     public static void main(String[] args) {
@@ -20,7 +18,14 @@ public class OptionalClassPractice05 {
         employee0211List.add(emp5);
 
         Employee0211 emp = findEmployee(employee0211List, "Jane Doe", 37);
-        System.out.println(emp.toString());
+        System.out.println(emp.getEmployeeName() + ", " + emp.getEmployeeAge());
+
+        Optional<Employee0211> emp_2 = findEmployee2(employee0211List, "Null Pointer", 0);
+        if ( emp_2.isPresent() ) {
+            System.out.println(emp_2.get().getEmployeeName() + ", " + emp_2.get().getEmployeeAge());
+        } else {
+            System.out.println("Employee not found");
+        }
     }
 
     public static Employee0211 findEmployee(List<Employee0211> empList, String name, int age) {
@@ -31,16 +36,40 @@ public class OptionalClassPractice05 {
         }
         return null;
     }
+
+    // handling null pointer exception by using optional
+    public static Optional<Employee0211> findEmployee2(List<Employee0211> empList, String name, int age) {
+        for ( Employee0211 e: empList ) {
+            if ( e.getEmployeeName().equalsIgnoreCase(name) && e.getEmployeeAge() == age ) {
+                return Optional.of(e);
+            }
+        }
+        return Optional.empty();
+    }
 }
 
-@Setter
-@Getter
 class Employee0211 {
     private String employeeName;
     private int employeeAge;
 
     public Employee0211(String employeeName, int employeeAge) {
         this.employeeName = employeeName;
+        this.employeeAge = employeeAge;
+    }
+
+    public String getEmployeeName() {
+        return this.employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public int getEmployeeAge() {
+        return this.employeeAge;
+    }
+
+    public void setEmployeeAge(int employeeAge) {
         this.employeeAge = employeeAge;
     }
 }

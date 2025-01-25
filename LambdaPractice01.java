36d95c8ae50669759d10bb11ffbd246dade9d6d6
Paranjyoti Mohanty <paranjyoti.mohanty@gmail.com>
2025-01-25 23:57:51 +0530

    Changing the code flow so as to apply JUnit test on this file

--- LambdaPractice01.java
+++ LambdaPractice01.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class LambdaPractice01 {
     public static void main(String[] args) {
@@ -9,8 +10,21 @@ public class LambdaPractice01 {
         numbers.add(10);
         numbers.add(14);
         System.out.println(numbers);
-        numbers.forEach( n -> System.out.print((n + 2) + " ") );
-        System.out.println("-----------------------------");
-        numbers.forEach( n -> System.out.print((n * n) + " ") );
+
+        LambdaPractice01 practice01 = new LambdaPractice01();
+        System.out.println("Add 2 to all elements: " + practice01.addTwo(numbers));
+        System.out.println("Square all the elements: " + practice01.square(numbers));
+    }
+
+    public List<Integer> addTwo(List<Integer> numbers) {
+        List<Integer> result = new ArrayList<>();
+        numbers.forEach(n -> result.add(n + 2));
+        return result;
+    }
+
+    public List<Integer> square(List<Integer> numbers) {
+        List<Integer> result = new ArrayList<>();
+        numbers.forEach(n -> result.add(n * n));
+        return result;
     }
 }

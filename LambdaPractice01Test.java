dcebe538c036dfe9c776920f329e19937dbaf431
Paranjyoti Mohanty <paranjyoti.mohanty@gmail.com>
2025-01-30 03:41:37 +0530

    Working unit test demo with the help of JUnit5

--- LambdaPractice01Test.java
+++ LambdaPractice01Test.java
@@ -1,10 +1,11 @@
-
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LambdaPractice01Test {
 
+    @Test
     public void testAddTwo() {
         LambdaPractice01 practice01 = new LambdaPractice01();
         List<Integer> input = new ArrayList<>();
@@ -19,7 +20,7 @@ public class LambdaPractice01Test {
         expected.add(3);
         expected.add(6);
         expected.add(12);
-        expected.add(16);
+        expected.add(14);
 
         List<Integer> actual = practice01.addTwo(input);
         assertEquals(expected, actual, "The lists should match after adding two to each element");

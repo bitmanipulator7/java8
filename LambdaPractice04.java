5411bfd3917c5b6b269280e25a355f9bdf899135
Paranjyoti Mohanty <paranjyoti.mohanty@gmail.com>
2025-02-01 01:36:49 +0530

    Method Reference refers to a method; and not to it's return

--- LambdaPractice04.java
+++ LambdaPractice04.java
@@ -1,3 +1,5 @@
+import java.util.function.Supplier;
+
 public class LambdaPractice04 {
     public static void main(String[] args) {
         ExampleFunctionalInterface0372 functionalInterface0372 = new ExampleFunctionalInterface0372() {
@@ -9,6 +11,15 @@ public class LambdaPractice04 {
 
         ExampleFunctionalInterface0372 functionalInterface03721 = () -> "How are you doing?";
         System.out.println(functionalInterface03721.behavior0372());
+        // String output03721 = functionalInterface03721::behavior0372;
+        // the above statement would result in an error because,
+        // the method reference (functionalInterface03721::behavior0372) refers to a method, not the result of that method.
+
+        Supplier<String> supplier = functionalInterface03721::behavior0372;
+        System.out.println(supplier.get());
+
+        Supplier<String> supplier1 = functionalInterface0372::behavior0372;
+        System.out.println(supplier1.get());
     }
 }
 
